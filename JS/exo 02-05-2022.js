d271feb1c5cf1c194628016924d422af2937d05b
Mathieu971 <mathieu.atout@lepoles.org>
2022-05-06 16:56:24 +0200

    06-05-2022

---
+++ JS/exo 02-05-2022.js
@@ -0,0 +1,229 @@
+// var nom;
+// nom = "Mathiouw";
+// alert(nom);
+// console.log(nom);
+// prompt("Yo What's Up", nom);
+// var jour;
+// jour = prompt("Quel jour de la semaine ?");
+// document.write(jour);
+// console.log(jour);
+// var mon_exercice = "je suis en train de faire mon exercice";
+// document.write(mon_exercice);
+// alert(mon_exercice);
+// var affichage = prompt("as-tu réussis ton exercice?");
+// document.write(affichage);
+// var monChiffre = 2022;
+// document.write(typeof monChiffre);
+// var monDecim;
+// monDecim = 3.14;
+// document.write(typeof monDecim);
+// var maChainedeCaractere = "57.12";
+// console.log(typeof maChainedeCaractere);
+// var maChainedeCaractere2 = parseFloat(maChainedeCaractere);
+// console.log(typeof maChainedeCaractere);
+// var maChainedeCaractere3 = parseInt(maChainedeCaractere2);
+// console.log(maChainedeCaractere);
+// console.log(typeof maChainedeCaractere3);
+// var maChainedeCaractere4 = maChainedeCaractere3.toString();
+// console.log(typeof maChainedeCaractere4);
+
+// document.write(
+//   maChainedeCaractere +
+//     "-" +
+//     maChainedeCaractere2 +
+//     "-" +
+//     maChainedeCaractere3 +
+//     "-" +
+//     maChainedeCaractere4
+// );
+// var annee = 2017;
+// var futur = 3;
+// var calcul = annee + futur;
+// console.log(calcul);
+// document.write(calcul + "&nbsp&nbsp" + calcul + "<br>");
+// document.write(calcul + "<br>");
+// var nb1, nb2, resultat;
+// nb1 = 10;
+// nb2 = 5;
+
+// resultat = nb1 + nb2;
+// console.log(resultat);
+// document.write(resultat);
+// var resultatSoustraction = nb1 - nb2;
+// console.log(resultatSoustraction);
+// var resultMultiplication = nb1 * nb2;
+// console.log(resultMultiplication);
+// var resultatDivision = nb1 / nb2;
+// console.log(resultatDivision);
+// var resultatModulo = nb1 % nb2;
+// console.log(resultatModulo);
+// var chi1, chi2;
+// chi1 = 12;
+// chi2 = 3;
+// // document.write(chi1 + "&nbsp" + chi2);
+// // var multiplication;
+// // multiplication = 15 * 10;
+// // prompt(multiplication);
+// // var modulo = chi1 % chi2;
+// // document.write(modulo);
+// chi2 = chi2.toString(chi2);
+// console.log(typeof chi2);
+
+// var prix;
+// prix = prompt("Quel Prix ?");
+// var dollars = 1.05;
+// var resultat = prix * dollars;
+// console.log(resultat);
+// document.write(resultat);
+
+// var Chambre1 = 10;
+// var Chambre2 = 50;
+// var Random;
+// Random = Chambre2;
+// Chambre2 = Chambre1;
+// Chambre1 = Random;
+// document.write(Chambre2);
+// var Random = prompt("Un nombre ?");
+// var Prenom = "Mathiouw";
+// if (Random > 10) {
+//   document.write(Prenom);
+// }
+// var Prenom = "Mathieu";
+// var Mat = prompt("Un prenom");
+// if (Mat == Prenom) {
+//   document.write("Mathieu");
+// } else {
+//   document.write("C'est pas mon blaze IDIOT!");
+// }
+// var chiffre = prompt("Entre un chiffre");
+// var resultat = chiffre % 5;
+// if (resultat == 0) {
+//   document.write("C'est bien un multiple de 5");
+// } else {
+//   document.write("Pas un multplie de FIVE");
+// }
+// var nb2 = Number(prompt("Entrez nb2 :"));
+// // document.write(nb2);
+// document.write(typeof nb2);
+// var Magie = prompt("Qui est le Boss ?");
+// alert("Le Boss est" + " " + Magie);
+// var resultat = prompt("Ecris un Chiffre");
+// var calcul = resultat % 13;
+// if (calcul == 0) {
+//   document.write("c'est bien un multiple de " + " " + resultat);
+// } else {
+//   document.write("Ce n'est pas un multiple de 13");
+// }
+// var Magie = prompt("Quoi de neuf");
+// var calcul = Magie % 7;
+// parseInt(calcul);
+// if (calcul == 0) {
+//   document.write("C'est un multiple de 7" + " " + typeof calcul);
+// } else {
+//   document.write(Magie + " " + typeof Magie);
+// }
+// var Zzz = prompt("Une valeur ?");
+// var calcul = Zzz * 1.8 + 32;
+// document.write("la valeur que tu as saisis =" + " " + calcul + "<br>");
+// var cac = calcul % 3;
+// document.write("Le modulo du resultat precedent est :" + " " + cac);
+// var dia = prompt("Valeur 1");
+// var dia2 = prompt("Valeur 2");
+// var Permutation;
+// Permutation = dia2;
+// dia2 = dia;
+// dia = Permutation;
+// document.write(dia + "<br>");
+// document.write(dia2 + "<br>");
+// for (var yo = 1; yo <= 20; yo += 2) {
+//   document.write(yo + " ");
+// }
+// var yosh = prompt("Valeur ?");
+// while (yosh <= 20) {
+//   yosh = yosh + 2;
+//   document.write(yosh);
+// }
+// var Magie = 1;
+// while (Magie <= 10) {
+//   document.write("Ton niveau de magie est de :" + " " + Magie + "<br>");
+//   Magie += 1;
+// }
+// var fruit = "";
+// while (fruit != "Fraise") {
+//   fruit = prompt("Un fruit ?");
+//   document.write("<hr/><p>La valeur n'est pas le bon fruit</p>");
+// }
+// document.write("<hr/><p>C'est bien le fruit demandé : " + " " + fruit);
+// var cash = 0;
+// var mois = 0;
+// while (cash < 1000) {
+//   cash = cash + 50;
+//   mois = mois + 1;
+//   document.write(
+//     "<hr/><p>Temps en mois :" +
+//       mois +
+//       " " +
+//       "J'ai actuellement" +
+//       " " +
+//       cash +
+//       "$</p>"
+//   );
+// }
+// document.write(
+//   "<hr/>Ca y est j'ai actuellement :" +
+//     "au bout de " +
+//     mois +
+//     "mois" +
+//     " " +
+//     cash +
+//     "$"
+// );
+// var temps = 0;
+// for (sous = 1000; sous <= 2000; sous += 50) {
+//   temps++;
+//   document.write(
+//     "ce mois j'ai " + sous + " " + "en" + " " + temps + " " + "mois" + "<br>"
+//   );
+// }
+// var maj = 18;
+// var age = parseInt(prompt("Quel age as tu ?"));
+// while (age >= 3 || age < 120) {
+//   age = parseInt(prompt("Quel age as tu ?"));
+//   if (age > 3 && age < maj) {
+//     alert("Tu es Mineur !");
+//   }
+//   if (age >= maj && age <= 120) {
+//     alert("Bienvenue Amigo ! fais comme chez toi :D");
+//     // document.location.href = "https://www.youtube.com/";
+//   }
+// }
+var jour = prompt("date ?");
+switch (jour) {
+  case "lundi":
+    document.write("Demain on est Mardi");
+    break;
+
+  case "mardi":
+    document.write("Demain on est mercredi");
+    break;
+
+  case "mercredi":
+    document.write("Demain on est jeudi");
+    break;
+
+  case "jeudi":
+    document.write("Demain on est vendredi");
+    break;
+
+  case "vendredi":
+    document.write("Demain on est samedi");
+    break;
+
+  case "samedi":
+    document.write("Demain on est dimanche");
+    break;
+
+  case "dimanche":
+    document.write("Demain on est lundi");
+    break;
+}
